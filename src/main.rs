c1526c41c5b2da8a1b0ac39aa6f3a439489c5dea
guilhem-sante <76043368+guilhem-sante@users.noreply.github.com>
2024-10-25 17:23:32 +0200

    draft: game impl
    Co-authored-by: Guilhem Santé <guilhem-sante@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,19 @@
-// Todo
+// Todos
 //
-// - Structure board
+// X Structure board
+// X Une fonction pour placer un pion
+// X Une fonction pour print le board
+// X System de tour par tour (Faire une structure game)
 // - Une fonction pour checker la win
-// - Une fonction pour placer un pion
-// - System de tour par tour
-// -
-use std::{error::Error, fmt};
+// - Faire une CLI
+// - Faire une commande pour jouer depuis la CLI
+// X Restructurer en module
 
-#[derive(Debug, Clone)]
-pub enum CellStatus
-{
-    Available,
-    Black,
-    White,
-}
-#[derive(Debug)]
-pub struct Board
-{
-    #[allow(dead_code)]
-    size: u8,
-    cells: Vec<Vec<CellStatus>>,
-}
-
-impl Board
-{
-    fn new(size: u8) -> Self
-    {
-        let cells = (0..size)
-            .map(|_| vec![CellStatus::Available; size as usize])
-            .collect::<Vec<Vec<CellStatus>>>();
-
-        Board { size, cells }
-    }
-
-    fn _set_cell(
-        &mut self,
-        x: u8,
-        y: u8,
-        new_status: CellStatus,
-    ) -> Result<(), Box<dyn Error>>
-    {
-        if x > self.size || y > self.size {
-            return Err(format!("error {} or {} > {}", x, y, self.size).into());
-        }
-        self.cells[x as usize][y as usize] = new_status;
-        Ok(())
-    }
-}
-
-impl fmt::Display for Board
-{
-    fn fmt(
-        &self,
-        f: &mut fmt::Formatter,
-    ) -> fmt::Result
-    {
-        let board_as_string: String = self
-            .cells
-            .iter()
-            .map(|row| {
-                row.iter()
-                    .map(|cell| {
-                        match cell {
-                            CellStatus::Available => " ",
-                            CellStatus::Black => "X",
-                            CellStatus::White => "O",
-                        }
-                    })
-                    .collect::<Vec<_>>()
-                    .join("|")
-            })
-            .collect::<Vec<_>>()
-            .join("\n");
-
-        writeln!(f, "{}", board_as_string)
-    }
-}
+mod board;
+mod game;
+use game::*;
 
 fn main()
 {
     println!("Welcome gomokurs!");
-    let b = Board::new(10);
-    println!("{b}");
 }
