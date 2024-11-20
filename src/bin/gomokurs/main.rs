fb0ebf8b7d8d7684b3bd188df713a3c5d493cd4d
guilhem-sante <76043368+guilhem-sante@users.noreply.github.com>
2024-11-20 18:08:27 +0100

    refactor: subprocess creation and attachment without fifo files

--- src/bin/gomokurs/main.rs
+++ src/bin/gomokurs/main.rs
@@ -1,3 +1,12 @@
+use gomokurs::outbound::NamedPipe;
+use std::path::Path;
+
 #[tokio::main]
 async fn main() {
+    let binary = Path::new("/home/ubuntu/Repositories/coding-kelps/gomokurs/.debug/main");
+
+    let mut pipe = match NamedPipe::new(binary).await {
+        Ok(p) => p,
+        Err(e) => return eprintln!("{}", e),
+    };
 }
