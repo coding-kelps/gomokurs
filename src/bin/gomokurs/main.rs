de4faad2bd0a7d31efb3c3652db674f353e326d4
guilhem-sante <76043368+guilhem-sante@users.noreply.github.com>
2024-11-23 13:05:08 +0100

    feat: add request_start for local program outbound

--- src/bin/gomokurs/main.rs
+++ src/bin/gomokurs/main.rs
@@ -3,14 +3,19 @@ use std::path::Path;
 
 #[tokio::main]
 async fn main() {
-    let binary = Path::new("./.debug/respondToAnythingWith3sDelay");
+    let binary = Path::new("./.debug/gomocku");
 
-    let mut pipe = match LocalProgram::new(binary).await {
-        Ok(p) => p,
+    let mut local = match LocalProgram::new(binary).await {
+        Ok(l) => l,
         Err(e) => return eprintln!("{}", e),
     };
 
-    match pipe.request_begin().await {
+    match local.request_start(20).await {
+        Ok(_) => (),
+        Err(e) => eprintln!("error {}", e),
+    }
+
+    match local.request_begin().await {
         Ok(p) => println!("player position: {}", p),
         Err(e) => eprintln!("error {}", e),
     }
