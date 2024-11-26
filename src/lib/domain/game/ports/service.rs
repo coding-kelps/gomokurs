dbd95f38bbe619c01348b8cf7f2585b50593182d
guilhem-sante <76043368+guilhem-sante@users.noreply.github.com>
2024-11-26 17:47:01 +0100

    refactor: explicit async fn of ports traits

--- src/lib/domain/game/ports/service.rs
+++ src/lib/domain/game/ports/service.rs
@@ -1,5 +1,7 @@
 use crate::domain::game::models::{GameEnd, PlayError, PlayRequest};
 
 pub trait GameService {
-    async fn play(&mut self, _req: &PlayRequest) -> Result<GameEnd, PlayError>;
+    fn play(
+        &mut self, _req: &PlayRequest
+    ) -> impl std::future::Future<Output = Result<GameEnd, PlayError>>;
 }
