632f698af35146fe91187ff5208429c33e67ddff
Guilhem Sante <guilhem.snt@gmail.com>
2024-11-27 16:42:04 +0100

    refactor: separate board and game models for clarity

--- src/lib/domain/game/models.rs
+++ src/lib/domain/game/models.rs
@@ -1,5 +1,7 @@
+mod game;
 mod board;
 mod requests;
 
+pub use game::*;
 pub use board::*;
 pub use requests::*;
--- src/lib/domain/game/ports/player_client.rs
+++ src/lib/domain/game/ports/player_client.rs
@@ -1,4 +1,4 @@
-use crate::domain::game::models::{Information, PlayerInformations, Position, RelativeTurn, RequestAboutError, RequestBeginError, RequestBoardError, RequestEndError, RequestInfoError, RequestStartError, RequestTurnError};
+use crate::domain::game::models::*;
 
 pub trait PlayerClient {
     fn request_start(
