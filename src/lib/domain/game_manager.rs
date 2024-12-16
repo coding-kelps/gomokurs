9a24657bfc26cf898f9a720362e2240bcb17ddae
guilhem-sante <76043368+guilhem-sante@users.noreply.github.com>
2024-12-16 19:01:23 +0100

    doc: rustdoc for adapters and domain

--- src/lib/domain/game_manager.rs
+++ src/lib/domain/game_manager.rs
@@ -1,3 +1,13 @@
+//! Player actions and timers handling.
+//! 
+//! This module defines the Game Manager service, including the
+//! [`ports::GameManagerService`] trait and the [`service::Service`]
+//! implementation.
+//! 
+//! The Game Manager is responsible for handling actions taken by players, 
+//! notifying them when necessary, and managing their respective timers.
+
+
 pub mod models;
 pub mod ports;
 pub mod service;
