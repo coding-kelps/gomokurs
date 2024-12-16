b6c94018a870746c1bbe70976c566843c7ac97d1
Guilhem Santé <guilhem.snt@gmail.com>
2024-12-16 19:01:23 +0100

    doc: rustdoc for adapters and domain

--- src/lib/adapters.rs
+++ src/lib/adapters.rs
@@ -1,1 +1,6 @@
+//! Crate domain's ports implementation
+//! 
+//! This module defines the crate's adapters meaning the implementation to 
+//! domain's inbound and outbound ports according to Hexagonal Architecture.
+
 pub mod player_interfaces;
