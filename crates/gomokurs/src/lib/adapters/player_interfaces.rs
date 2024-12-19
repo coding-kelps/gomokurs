f4a8f03c16139b9953e49378c35aea3a7209f8d2
Guilhem Santé <guilhem.snt@gmail.com>
2024-12-19 17:01:08 +0100

    refactor: set local player interface as a crate feature

--- crates/gomokurs/src/lib/adapters/player_interfaces.rs
+++ crates/gomokurs/src/lib/adapters/player_interfaces.rs
@@ -1,6 +1,5 @@
 //! This module contains the different adapters of the player interface
 //! (implementing both PlayerListener and PlayerNotifier).
 
+#[cfg(feature = "local")]
 pub mod local;
-
-pub use local::Local;
