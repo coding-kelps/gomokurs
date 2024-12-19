c8fca5f3d6aa215ba31479e4b8599ad6e56f3e0e
guilhem-sante <76043368+guilhem-sante@users.noreply.github.com>
2024-12-19 17:01:08 +0100

    refactor: set local player interface as a crate feature

--- crates/gomokurs/src/bin/gomokurs/main.rs
+++ crates/gomokurs/src/bin/gomokurs/main.rs
@@ -1,6 +1,6 @@
 use std::path::Path;
 use gomokurs::adapters::player_interfaces::local::local::CreateLocalProgramError;
-use gomokurs::adapters::player_interfaces::Local;
+use gomokurs::adapters::player_interfaces::local::Local;
 use gomokurs::domain::board_state_manager::{BoardStateManager, models::BoardSize};
 use gomokurs::domain::game_manager::GameManager;
 use gomokurs::domain::player_interfaces_manager::{PlayerInterfacesManager, PlayerInterfacesManagerService};
