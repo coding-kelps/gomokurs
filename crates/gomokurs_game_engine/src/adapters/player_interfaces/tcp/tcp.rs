35e27b4320c68f87b716a8488f0d4d720578a5e8
guilhem-sante <76043368+guilhem-sante@users.noreply.github.com>
2025-01-27 17:54:56 +0100

    feat: add tcp player interface notifier port adapter

--- crates/gomokurs_game_engine/src/adapters/player_interfaces/tcp/tcp.rs
+++ crates/gomokurs_game_engine/src/adapters/player_interfaces/tcp/tcp.rs
@@ -1,29 +1,27 @@
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
 use thiserror::Error;
+use std::sync::Arc;
+use tokio::sync::Mutex;
 
 pub struct Tcp {
-    black_player_client: TcpStream,
-    white_player_client: TcpStream,
+    pub writer: Arc<Mutex<TcpStream>>,
 }
 
 pub struct CreateTcpInterfaceConfiguration {
-    pub black_player_address: String,
-    pub white_player_address: String,
+    pub player_address: String,
 }
 
 #[derive(Debug, Error)]
 pub enum CreateTcpInterfaceError {
     /// An error occurred when spawning the AI binary as a subprocess.
-    #[error("create subprocess error: `{0}`")]
-    CreateSubprocessError(#[from] tokio::io::Error),
+    #[error("create tcp client error: `{0}`")]
+    CreateClientError(#[from] tokio::io::Error),
 }
 
 impl Tcp {
     pub async fn new(cfg: CreateTcpInterfaceConfiguration) -> Result<Self, CreateTcpInterfaceError> {
         Ok(Self {
-            black_player_client: TcpStream::connect(cfg.black_player_address).await?,
-            white_player_client: TcpStream::connect(cfg.white_player_address).await?,
+            writer: Arc::new(Mutex::new(TcpStream::connect(cfg.player_address).await?)),
         })
     }
 }
