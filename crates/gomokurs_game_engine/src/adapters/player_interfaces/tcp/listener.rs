d8405d8d19d259e827611fafd6b54d0afa28eca9
Guilhem Sante <guilhem.snt@gmail.com>
2025-01-27 20:14:24 +0100

    feat: tcp adapter

--- crates/gomokurs_game_engine/src/adapters/player_interfaces/tcp/listener.rs
+++ crates/gomokurs_game_engine/src/adapters/player_interfaces/tcp/listener.rs
@@ -1,19 +1,155 @@
 //! This module contains the implementation of the PlayerListener port for the
 //! tcp player interface.
 
-use crate::adapters::player_interfaces::tcp::Tcp;
+use crate::adapters::player_interfaces::tcp::tcp::{Tcp, ActionID};
 use crate::domain::player_interfaces_manager::ports::PlayerListener;
 use crate::domain::player_interfaces_manager::models::ListenError;
-use crate::domain::game_manager::models::{PlayerColor, PlayerAction};
+use crate::domain::game_manager::models::{PlayerColor, PlayerAction, Position, PlayerDescription};
 use tokio::sync::mpsc::Sender;
+use tokio::io::{self, AsyncReadExt};
+use std::collections::HashMap;
+use anyhow::anyhow;
 
 impl PlayerListener for Tcp {
-    #[allow(unused_variables)]
     async fn listen(
         &self,
         player: PlayerColor,
         tx: Sender<(PlayerColor, PlayerAction)>,
     ) -> Result<(), ListenError> {
-        Ok(())
+        let mut buf = vec![0u8; 1];
+
+        loop {
+            let mut reader = self.reader.lock().await;
+
+            reader.read_exact(&mut buf)
+                .await
+                .map_err(|e| ListenError::Unknown(anyhow!(e)))?;
+
+            let action = match buf[0] {
+                ActionID::READY => self.ready_handler()
+                    .await
+                    .map_err(|e| ListenError::Unknown(anyhow!(e)))?,
+                ActionID::PLAY => self.play_handler()
+                    .await
+                    .map_err(|e| ListenError::Unknown(anyhow!(e)))?,
+                ActionID::PLAYER_DESCRIPTION => self.player_description_handler()
+                    .await
+                    .map_err(|e| ListenError::Unknown(anyhow!(e)))?,
+                ActionID::UNKNOWN => self.unknown_handler()
+                    .await
+                    .map_err(|e| ListenError::Unknown(anyhow!(e)))?,
+                ActionID::ERROR => self.error_handler()
+                    .await
+                    .map_err(|e| ListenError::Unknown(anyhow!(e)))?,
+                ActionID::MESSAGE => self.message_handler()
+                    .await
+                    .map_err(|e| ListenError::Unknown(anyhow!(e)))?,
+                ActionID::DEBUG => self.debug_handler()
+                    .await
+                    .map_err(|e| ListenError::Unknown(anyhow!(e)))?,
+                ActionID::SUGGESTION => self.suggestion_handler()
+                    .await
+                    .map_err(|e| ListenError::Unknown(anyhow!(e)))?,
+                _ => continue,
+            };
+
+            tx.send((player, action)).await.expect("failed to send to channel");
+        }
+    }
+}
+
+impl Tcp {
+    async fn ready_handler(
+        &self,
+    ) -> Result<PlayerAction, io::Error> {
+        Ok(PlayerAction::Ok)
+    }
+
+    async fn play_handler(
+        &self,
+    ) -> Result<PlayerAction, io::Error> {
+        let mut reader = self.reader.lock().await;
+
+        let mut buf = [0u8; 2];
+
+        reader.read_exact(&mut buf).await?;
+
+        Ok(PlayerAction::Play(Position::new(buf[0], buf[1])))
+    }
+
+    async fn player_description_handler(
+        &self,
+    ) -> Result<PlayerAction, io::Error> {
+        let info = HashMap::new();
+
+        Ok(PlayerAction::Description(PlayerDescription{
+            info: info,
+        }))
+    }
+
+    async fn unknown_handler(
+        &self,
+    ) -> Result<PlayerAction, io::Error> {
+        Ok(PlayerAction::Unknown(String::from("")))
+    }
+
+    async fn error_handler(
+        &self,
+    ) -> Result<PlayerAction, io::Error> {
+        let mut reader = self.reader.lock().await;
+
+        let mut buf = [0u8; 4];
+        reader.read_exact(&mut buf).await?;
+        let payload_size = u32::from_be_bytes(buf) as usize;
+
+        let mut buf = vec![0u8; payload_size];
+        reader.read_exact(&mut buf).await?;
+        let msg = String::from_utf8(buf).expect("from utf-8 error");
+
+        Ok(PlayerAction::Error(msg))
+    }
+
+    async fn message_handler(
+        &self,
+    ) -> Result<PlayerAction, io::Error> {
+        let mut reader = self.reader.lock().await;
+
+        let mut buf = [0u8; 4];
+        reader.read_exact(&mut buf).await?;
+        let payload_size = u32::from_be_bytes(buf) as usize;
+
+        let mut buf = vec![0u8; payload_size];
+        reader.read_exact(&mut buf).await?;
+        let msg = String::from_utf8(buf).expect("from utf-8 error");
+
+        Ok(PlayerAction::Message(msg))
+    }
+
+    async fn debug_handler(
+        &self,
+    ) -> Result<PlayerAction, io::Error> {
+        let mut reader = self.reader.lock().await;
+
+        let mut buf = [0u8; 4];
+        reader.read_exact(&mut buf).await?;
+        let payload_size = u32::from_be_bytes(buf) as usize;
+
+        let mut buf = vec![0u8; payload_size];
+        reader.read_exact(&mut buf).await?;
+        let msg = String::from_utf8(buf).expect("from utf-8 error");
+
+        Ok(PlayerAction::Debug(msg))
+    }
+
+    async fn suggestion_handler(
+        &self,
+    ) -> Result<PlayerAction, io::Error> {
+        let mut reader = self.reader.lock().await;
+
+        let mut buf = [0u8; 2];
+
+        reader.read_exact(&mut buf).await?;
+
+        Ok(PlayerAction::Suggestion(Position::new(buf[0], buf[1])))
     }
 }
--- crates/gomokurs_game_engine/src/adapters/player_interfaces/tcp/tcp.rs
+++ crates/gomokurs_game_engine/src/adapters/player_interfaces/tcp/tcp.rs
@@ -1,10 +1,12 @@
 use tokio::net::TcpStream;
+use tokio::io::{ReadHalf, WriteHalf, split};
 use thiserror::Error;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
 pub struct Tcp {
-    pub writer: Arc<Mutex<TcpStream>>,
+    pub reader: Arc<Mutex<ReadHalf<TcpStream>>>,
+    pub writer: Arc<Mutex<WriteHalf<TcpStream>>>,
 }
 
 pub struct CreateTcpInterfaceConfiguration {
@@ -20,23 +22,35 @@ pub enum CreateTcpInterfaceError {
 
 impl Tcp {
     pub async fn new(cfg: CreateTcpInterfaceConfiguration) -> Result<Self, CreateTcpInterfaceError> {
+        let stream = TcpStream::connect(cfg.player_address).await?;
+        let (reader, writer) = split(stream);
+
         Ok(Self {
-            writer: Arc::new(Mutex::new(TcpStream::connect(cfg.player_address).await?)),
+            reader: Arc::new(Mutex::new(reader)),
+            writer: Arc::new(Mutex::new(writer)),
         })
     }
 }
 
 #[non_exhaustive]
-pub struct CommandID;
+pub struct ActionID;
 
-impl CommandID {
-    pub const START: u8 = 0x01;
-    pub const TURN: u8 = 0x02;
-    pub const BEGIN: u8 = 0x03;
-    pub const BOARD: u8 = 0x04;
-    pub const BOARD_TURN: u8 = 0x05;
-    pub const BOARD_END: u8 = 0x06;
-    pub const INFO: u8 = 0x07;
-    pub const END: u8 = 0x08;
-    pub const ABOUT: u8 = 0x09;
+impl ActionID {
+    pub const START: u8                 = 0x01;
+    pub const TURN: u8                  = 0x02;
+    pub const BEGIN: u8                 = 0x03;
+    pub const BOARD: u8                 = 0x04;
+    pub const BOARD_TURN: u8            = 0x05;
+    pub const BOARD_END: u8             = 0x06;
+    pub const INFO: u8                  = 0x07;
+    pub const END: u8                   = 0x08;
+    pub const ABOUT: u8                 = 0x09;
+    pub const READY: u8                 = 0x0A;
+    pub const PLAY: u8                  = 0x0B;
+    pub const PLAYER_DESCRIPTION: u8    = 0x0C;
+    pub const UNKNOWN: u8               = 0x0D;
+    pub const ERROR: u8                 = 0x0E;
+    pub const MESSAGE: u8               = 0x0F;
+    pub const DEBUG: u8                 = 0x10;
+    pub const SUGGESTION: u8            = 0x11;
 }
