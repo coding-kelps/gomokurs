97279ca0a25bd34bad7ef6ee3078893018dc1cc2
guilhem-sante <76043368+guilhem-sante@users.noreply.github.com>
2025-01-28 17:56:31 +0100

    feat: update TCP protocol

--- crates/gomokurs_game_engine/src/adapters/player_interfaces/tcp/tcp.rs
+++ crates/gomokurs_game_engine/src/adapters/player_interfaces/tcp/tcp.rs
@@ -36,21 +36,28 @@ impl Tcp {
 pub struct ActionID;
 
 impl ActionID {
-    pub const START: u8                 = 0x01;
-    pub const TURN: u8                  = 0x02;
-    pub const BEGIN: u8                 = 0x03;
-    pub const BOARD: u8                 = 0x04;
-    pub const BOARD_TURN: u8            = 0x05;
-    pub const BOARD_END: u8             = 0x06;
-    pub const INFO: u8                  = 0x07;
-    pub const END: u8                   = 0x08;
-    pub const ABOUT: u8                 = 0x09;
-    pub const READY: u8                 = 0x0A;
-    pub const PLAY: u8                  = 0x0B;
-    pub const PLAYER_DESCRIPTION: u8    = 0x0C;
-    pub const UNKNOWN: u8               = 0x0D;
-    pub const ERROR: u8                 = 0x0E;
-    pub const MESSAGE: u8               = 0x0F;
-    pub const DEBUG: u8                 = 0x10;
-    pub const SUGGESTION: u8            = 0x11;
+    // Action that can be send from the manager to the player.
+
+    pub const MANAGER_PROTOCOL_COMPATIBLE: u8   = 0x00;
+    pub const MANAGER_START: u8                 = 0x01;
+    pub const MANAGER_TURN: u8                  = 0x02;
+    pub const MANAGER_BEGIN: u8                 = 0x03;
+    pub const MANAGER_BOARD: u8                 = 0x04;
+    pub const MANAGER_INFO: u8                  = 0x05;
+    pub const MANAGER_END: u8                   = 0x06;
+    pub const MANAGER_ABOUT: u8                 = 0x07;
+    pub const MANAGER_UNKNOWN: u8               = 0x08;
+    pub const MANAGER_ERROR: u8                 = 0x09;
+
+    // Actions that can be send from the player to the manager.
+
+    pub const PLAYER_PROTOCOL_VERSION: u8       = 0x0A;
+    pub const PLAYER_READY: u8                  = 0x0B;
+    pub const PLAYER_PLAY: u8                   = 0x0C;
+    pub const PLAYER_PLAYER_DESCRIPTION: u8     = 0x0D;
+    pub const PLAYER_UNKNOWN: u8                = 0x0E;
+    pub const PLAYER_ERROR: u8                  = 0x0F;
+    pub const PLAYER_MESSAGE: u8                = 0x10;
+    pub const PLAYER_DEBUG: u8                  = 0x11;
+    pub const PLAYER_SUGGESTION: u8             = 0x12;
 }
