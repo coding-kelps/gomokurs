ec5730a6dd4ddfa589bf768930600512fd5a97f1
guilhem-sante <76043368+guilhem-sante@users.noreply.github.com>
2025-02-11 14:35:18 +0100

    feat: add gomokurs mode to CLI parameters

--- crates/gomokurs_cli/src/configuration/cli.rs
+++ crates/gomokurs_cli/src/configuration/cli.rs
@@ -1,5 +1,14 @@
 use std::path::PathBuf;
 use clap::Parser;
+use gomokurs_coordinator::domain::coordinator::models::Mode;
+
+fn parse_mode(s: &str) -> Result<Mode, String> {
+    match s.to_lowercase().as_str() {
+        "single" => Ok(Mode::SingleGame),
+        "loop"   => Ok(Mode::Loop),
+        _ => Err(format!("Invalid mode: {}", s)),
+    }
+}
 
 #[derive(Parser, Debug, Clone)]
 pub struct Cli {
@@ -20,6 +29,11 @@ pub struct Cli {
     #[arg(long)]
     #[arg(default_value="INFO")]
     pub log_level: String,
+
+    #[arg(long)]
+    #[arg(default_value="single")]
+    #[arg(value_parser = parse_mode)]
+    pub mode: Mode,
 }
 
 
